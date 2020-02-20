f86eaaac20358219e3560ef8546cafe95c0f0060
i-arevalo <60589464+i-arevalo@users.noreply.github.com>
2020-02-19 22:39:02 -0600

    Se crea el archivo header de la librería USART

---
+++ USART.h
@@ -0,0 +1,10 @@
+#ifndef __USART_H_
+#define	__USART_H_
+
+#include <xc.h> // include processor files - each processor file is guarded. 
+
+void transmision_conf(void);
+void recepcion_conf(void);
+void transmitir (char*);
+
+#endif	/* XC_HEADER_TEMPLATE_H */
